b73819dc7ae2164ac790574e0223131fb80d4093
nadiraazhar <141995122+nadiraazhar@users.noreply.github.com>
2023-08-12 22:59:55 +0800

    Update AdvertisingApp.py

--- AdvertisingApp.py
+++ AdvertisingApp.py
@@ -21,3 +21,14 @@ def user_input_features():
     features = pd.DataFrame(data, index=[0])
     return features
 
+df = user_input_features()
+
+st.subheader('User Input parameters')
+st.write(df)
+
+loaded_model = pickle.load(open("AdvertisingRegression.h5", "rb"))
+
+prediction = loaded_model.predict(df)
+
+st.subheader('Prediction')
+st.write(prediction)
