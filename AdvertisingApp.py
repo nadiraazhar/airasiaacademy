a1781fa312846f750ca602738c5df4656b21433d
nadiraazhar <141995122+nadiraazhar@users.noreply.github.com>
2023-08-12 22:58:50 +0800

    Update AdvertisingApp.py

--- AdvertisingApp.py
+++ AdvertisingApp.py
@@ -10,3 +10,14 @@ This app predicts the **number of sale** based on the method of advertising used
 """)
 
 st.sidebar.header('User Input Parameters')
+
+def user_input_features():
+    TV = st.sidebar.slider('TV', 0.0, 300.0, 150.0)
+    Radio = st.sidebar.slider('Radio', 0.0, 50.0, 25.0)
+    Newspaper = st.sidebar.slider('Newspaper', 0.0, 115.0, 75.0)
+    data = {'TV': TV,
+            'Radio': Radio,
+            'Newspaper': Newspaper}
+    features = pd.DataFrame(data, index=[0])
+    return features
+
